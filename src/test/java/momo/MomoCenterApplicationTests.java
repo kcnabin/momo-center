3ebcf9924b4cfbc8f2f2711ec39708674e5d4286
kcnabin <kcnabinkc@gmail.com>
2024-07-31 07:28:00 +0545

    project momo-center initialized

---
+++ src/test/java/momo/MomoCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package momo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomoCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
